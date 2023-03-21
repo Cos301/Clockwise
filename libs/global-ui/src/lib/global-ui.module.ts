b5ab756dc4f4510e388d62d36b4f3703cd99eba3
thomasisebeck <thomasisebeck@gmail.com>
2023-03-21 10:42:14 +0200

    created a progress bar ui element for the time remaining

---
+++ libs/global-ui/src/lib/global-ui.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TimeBarComponent } from './time-bar/time-bar.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [TimeBarComponent],
+})
+export class GlobalUiModule {}
---
+++ libs/global-ui/src/lib/time-bar/time-bar.component.stories.ts
@@ -0,0 +1,21 @@
+import { moduleMetadata, Story, Meta } from '@storybook/angular';
+import { TimeBarComponent } from './time-bar.component';
+
+export default {
+  title: 'TimeBarComponent',
+  component: TimeBarComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [],
+    })
+  ],
+} as Meta<TimeBarComponent>;
+
+const Template: Story<TimeBarComponent> = (args: TimeBarComponent) => ({
+  props: args,
+});
+
+
+export const Primary = Template.bind({});
+Primary.args = {
+}
