e36c335d8d96d9ad1d32839822d149a02f98f63a
thomasisebeck <thomasisebeck@gmail.com>
2023-03-23 11:46:48 +0200

    created chats library

--- apps/global-ui-e2e/src/e2e/time-bar/time-bar.component.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('global-ui', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=timebarcomponent--primary'));
-  it('should render the component', () => {
-    cy.get('mp-time-bar').should('exist');
-  });
-});
