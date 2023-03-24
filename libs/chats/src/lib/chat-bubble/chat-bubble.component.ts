7cc61851be99601b27dfe5893ee3200fb7eee16c
thomasisebeck <thomasisebeck@gmail.com>
2023-03-24 13:34:03 +0200

    added ionic to message item

--- libs/chats/src/lib/chat-bubble/chat-bubble.component.ts
+++ libs/chats/src/lib/chat-bubble/chat-bubble.component.ts
@@ -12,9 +12,9 @@ export class ChatBubbleComponent {
   @Input() time: string;
   @Input() unreadCount: number;
   constructor() {
-    this.chatName = '<Chat Name>';
+    this.chatName = 'Chat Name>';
     this.profilePhotoUrl = 'profile-photo.png';
-    this.msgPreview = "Hi... I am a message preview and I will be displayed..... I want it to overflowl.................";
+    this.msgPreview = "Hi... I am a message preview and I will be displayed I want it to overflowl and displayer over two lines. Blah  Blah  Blah  Blah  Blah  Blah  Blah  Blah  Blah  Blah  Blah  Blah  Blah ";
     this.time = "14:45";
     this.unreadCount = 1;
   }
