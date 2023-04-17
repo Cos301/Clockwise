8efd60dc4f9d223288822eeefa792976a0b0d903
Jaco Malan <jacomalan.12@gmail.com>
2023-04-17 14:19:27 +0200

    Fixed api spec

--- libs/api/spec.ts
+++ libs/api/spec.ts
@@ -1,25 +1,29 @@
 /**
- * @api (Posts) {get} /api/posts/:start/:end Get posts within a range
+ * @api {get} /api/posts/:start/:end Get posts within a range
 *  @apiParam {Number} start Start of the range
 *  @apiParam {Number} end End of the range
+*  @apiGroup Posts
  */
 
 /**
- * @api (Posts) {get} /api/posts/:userId/:postId Get single post
+ * @api {get} /api/posts/:userId/:postId Get single post
 *  @apiParam {Number} userId The relevant user id (author of post).
 *  @apiParam {Number} postId The relevant post id.
+*  @apiGroup Posts
  */
 
 /**
- * @api (Posts) {patch} /api/posts/:ownerId/:userId/:postId Like a post
+ * @api {patch} /api/posts/:ownerId/:userId/:postId Like a post
 *  @apiParam {Number} userId The person liking the post
 *  @apiParam {Number} postId The id of the specific post made by the user.
 *  @apiParam {Number} ownerId The author of the post
+*  @apiGroup Posts
  */
 
 /**
- * @api (Posts) {put} /api/posts/:ownerId/:userId/:postId Add comment
+ * @api {put} /api/posts/:ownerId/:userId/:postId Add comment
 *  @apiParam {Number} ownerId Owner of the post
 *  @apiParam {Number} userId Commenter user id
 *  @apiParam {Number} postId The id of the specific post made by the user.
+*  @apiGroup Posts
  */
