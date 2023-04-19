d7ecc9515a98f118402d4dcbe9ebd7ad1acac3af
Jake Weatherhead <jakeweatherhead1@gmail.com>
2023-04-19 18:04:04 +0200

    Added "I" to Comment array

--- libs/api/posts/util/src/interfaces/posts.interface.ts
+++ libs/api/posts/util/src/interfaces/posts.interface.ts
@@ -1,10 +1,10 @@
 import { Timestamp } from 'firebase-admin/firestore';
-import { Comment } from './comments.interface';
+import { IComment } from './comments.interface';
 
 export interface IPost{
     post_id: string;
     caption: string;
-    comments: Comment[];
+    comments: IComment[];
     img_url: string;
     time_created: Timestamp;
     time_remove: Timestamp;
