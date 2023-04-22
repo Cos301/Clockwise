f0fa65110ea99ed3756065592acc7ad11373ca32
JNAIDOO <jessenaidoo24@gmail.com>
2023-04-22 18:09:20 +0200

    Fixed firestore get

--- libs/api/posts/data-access/src/posts.repository.ts
+++ libs/api/posts/data-access/src/posts.repository.ts
@@ -1,11 +1,18 @@
 import { Injectable } from "@nestjs/common"
 import * as admin from 'firebase-admin';
+import { collection, query, where } from "firebase/firestore";
 
 @Injectable()
 export class PostsRepository {
   //Fetch all posts from the database
   async fetchAllPosts() {
     console.log('Fetch all posts');
-    return await admin.firestore().collection('posts').get();
+
+    const postsRef  = admin.firestore().collection('posts');
+    console.log("🚀 ~ file: posts.repository.ts:12 ~ PostsRepository ~ fetchAllPosts ~ postsRef:", postsRef)
+    
+
+   // const data = await postsRef.get();
+    return postsRef;
   }
 }
