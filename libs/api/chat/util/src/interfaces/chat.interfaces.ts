e4eb7fe8e26890f07b414e2554a0b76b1b8d9cd3
Jake Weatherhead <jakeweatherhead1@gmail.com>
2023-04-24 14:00:36 +0200

    Updated interface to reflect document structure

--- libs/api/chat/util/src/interfaces/chat.interfaces.ts
+++ libs/api/chat/util/src/interfaces/chat.interfaces.ts
@@ -1,5 +1,8 @@
-
+import { IUser } from "@mp/api/users/util"
+import { Timestamp } from 'firebase-admin/firestore';
 
 export interface IChat {
-    chatId: string
+    chatId: string;
+    messages: [content: string, sender: IUser, time_created: Timestamp];
+    participants: IUser[];
 }
