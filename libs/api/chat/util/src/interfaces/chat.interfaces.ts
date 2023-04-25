a880ff31e6ce4fa9cea3a353b9a025c6f85c9fea
Jake Weatherhead <jakeweatherhead1@gmail.com>
2023-04-25 15:50:26 +0200

    Corrected array of maps specification

--- libs/api/chat/util/src/interfaces/chat.interfaces.ts
+++ libs/api/chat/util/src/interfaces/chat.interfaces.ts
@@ -3,6 +3,6 @@ import { Timestamp } from 'firebase-admin/firestore';
 
 export interface IChat {
     chatId: string;
-    messages: [content: string, sender: IUser, time_created: Timestamp];
+    messages: {content: string, sender: IUser, time_created: Timestamp}[];
     participants: IUser[];
 }
