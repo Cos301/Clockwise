d02b00d46d8aa1cf97982631ddb64479812372b4
JNAIDOO <jessenaidoo24@gmail.com>
2023-04-28 14:47:31 +0200

    Added request post changes

--- libs/api/posts/feature/src/commands/create-post.handler.ts
+++ libs/api/posts/feature/src/commands/create-post.handler.ts
@@ -13,28 +13,30 @@ export class CreatePostHandler implements ICommandHandler<CreatePostCommand> {
     
     console.log(`${CreatePostHandler.name}`);
 
-    // const request = command.request;
-    // const post_id = request.post.post_id;
-    // const caption = request.post.caption;
+    const request = command.request;
+    const post_id = request.post_id;
+    const caption = request.caption;
     // const comments = request.post.comments;
-    // const img_url = request.post.img_url;
-    // const time_created = request.post.time_created;
+    const img_url = request.img_url;
+    const time_created = Timestamp.now();
     // const time_remove = request.post.time_remove;
-    // const user_id = request.post.user_id;
-    
-    const post_id = "post_id_test";
-    const caption = "caption_test";
-    const comments : IComment[] = [];
-    const img_url = "img_url_test";
-    
-    const time_remove = Timestamp.fromDate(new Date());
-    const user_id = "1";
+    const user_id = request.user_id;
+    const postLife = request.postLife;
+    //const post_id = "post_id_test";
+    //const caption = "caption_test";
+
+    //const img_url = "img_url_test";
+    const dateRemoved = new Date();
+    dateRemoved.setHours(postLife);
+      
+      const time_remove = Timestamp.fromDate(dateRemoved);
+    //const user_id = "1";
     const data: IPost = {
         post_id,
         caption,
-        comments,
+        comments : [],
         img_url,
-        time_created: Timestamp.fromDate(new Date()),
+        time_created: time_created,
         time_remove,
         user_id
     };
