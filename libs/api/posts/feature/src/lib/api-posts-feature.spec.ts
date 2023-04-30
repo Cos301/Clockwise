d53f9174588db6ea466eacfb91ed2b9474e3c406
JNAIDOO <jessenaidoo24@gmail.com>
2023-04-30 17:01:38 +0200

    Unit testing for get all posts and Users

--- libs/api/posts/feature/src/lib/api-posts-feature.spec.ts
+++ libs/api/posts/feature/src/lib/api-posts-feature.spec.ts
@@ -5,7 +5,12 @@ import { PostsService } from '../posts.service';
 //Import all the handlers
 import { CreatePostHandler, CreateCommentHandler } from '../commands';
 import { GetAllPostsHandler, GetUserDataHandler } from '../queries';
+import { PostCreatedHandler } from '../events';
+import { Post } from '../models';
 import { PostsRepository } from '@mp/api/posts/data-access';
+import { IPost, GetUserDataQuery, GetAllPostsQuery } from '@mp/api/posts/util';
+import { Timestamp } from 'firebase-admin/firestore';
+import * as admin from 'firebase-admin';
 describe('apiPostsFeature', () => {
   let createPostHandler: CreatePostHandler;
   let createCommentHandler: CreateCommentHandler;
@@ -13,6 +18,7 @@ describe('apiPostsFeature', () => {
   let getUserDataHandler: GetUserDataHandler;
   let postsService: PostsService;
   let postsRepository: PostsRepository;
+  let publisher: EventPublisher;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -31,10 +37,12 @@ describe('apiPostsFeature', () => {
     createPostHandler = module.get<CreatePostHandler>(CreatePostHandler);
     createCommentHandler =
       module.get<CreateCommentHandler>(CreateCommentHandler);
-    getAllPostsHandler = module.get<GetAllPostsHandler>(GetAllPostsHandler);
+
     getUserDataHandler = module.get<GetUserDataHandler>(GetUserDataHandler);
     postsService = module.get<PostsService>(PostsService);
     postsRepository = module.get<PostsRepository>(PostsRepository);
+    publisher = module.get<EventPublisher>(EventPublisher);
+    getAllPostsHandler = new GetAllPostsHandler(postsRepository);
   });
 
   it('Create Post Handler defined', () => {
@@ -56,6 +64,81 @@ describe('apiPostsFeature', () => {
     expect(postsRepository).toBeDefined();
   });
 
+  //Create post command
+  it('Create Post', async () => {
+    const postTest: IPost = {
+      user_id: '1',
+      caption: 'test',
+      comments: [],
+      img_url: 'test',
+      post_id: '1',
+      time_created: Timestamp.now(),
+      time_remove: Timestamp.now(),
+    };
+    const fixture = new Post(
+      postTest.post_id,
+      postTest.caption,
+      postTest.comments,
+      postTest.img_url,
+      postTest.time_created,
+      postTest.time_remove,
+      postTest.user_id
+    );
 
-  
+    fixture.create = jest.fn();
+    fixture.commit = jest.fn();
+    publisher.mergeObjectContext = jest.fn((fixture) => fixture);
+    postsRepository.createPost = jest.fn();
+
+    // const createPostCommand = new PostCreatedHandler(postsRepository);
+    // const result = await createPostHandler.execute(createPostCommand);
+    // expect(result).toBeUndefined();
+    // expect(result.create).toHaveBeenCalled();
+  });
+
+  //Get all Posts
+  it('Get All Posts', async () => {
+    const postTest: IPost = {
+      user_id: '1',
+      caption: 'test',
+      comments: [],
+      img_url: 'test',
+      post_id: '1',
+      time_created: Timestamp.now(),
+      time_remove: Timestamp.now(),
+    };
+    postsRepository.fetchAllPosts = jest.fn(() => Promise.resolve([postTest]));
+
+    const result = await getAllPostsHandler.execute(new GetAllPostsQuery({}));
+    console.log(
+      '🚀 ~ file: api-posts-feature.spec.ts:113 ~ it ~ result:',
+      result
+    );
+
+    expect(result).toBeDefined();
+    expect(postsRepository.fetchAllPosts).toHaveBeenCalled();
+    expect(result.posts.length).toBe(1);
+    result.posts.forEach((x) => {
+      expect(x.caption).toBe(postTest.caption);
+      expect(x.comments).toBe(postTest.comments);
+      expect(x.img_url).toBe(postTest.img_url);
+      expect(x.post_id).toBe(postTest.post_id);
+      expect(x.time_created).toBe(postTest.time_created);
+      expect(x.time_remove).toBe(postTest.time_remove);
+      expect(x.user_id).toBe(postTest.user_id);
+    });
+
+
+  });
+
+  //Get User Data
+  it('Get User Data', async () => {
+    postsRepository.getUserData = jest.fn(() => Promise.resolve([]));
+    const result = await getUserDataHandler.execute(
+      new GetUserDataQuery({})
+    );
+    expect(result).toBeDefined();
+    expect(postsRepository.getUserData).toHaveBeenCalled();
+    expect(result.users.length).toBe(0);
+  });
 });
