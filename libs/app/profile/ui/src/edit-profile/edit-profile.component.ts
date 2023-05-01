bf09a9d770779457a1ff0ef24972a0742075b3ba
Francois Smith <90509905+francoissmith@users.noreply.github.com>
2023-05-01 22:11:38 +0200

    added edit-profile

---
+++ libs/app/profile/ui/src/edit-profile/edit-profile.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'edit-profile',
+  templateUrl: './edit-profile.component.html',
+  styleUrls: ['./edit-profile.component.scss'],
+})
+export class EditProfileComponent {}
