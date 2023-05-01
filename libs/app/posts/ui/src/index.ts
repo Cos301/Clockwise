6179679787b7b387bd224e9f1c72de12d87d0285
Francois Smith <90509905+francoissmith@users.noreply.github.com>
2023-05-01 22:06:23 +0200

    added user-profile imports

--- libs/app/posts/ui/src/index.ts
+++ libs/app/posts/ui/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/app-posts-ui';
 export * from './get-all-posts';
 export * from './create-post';
 export * from './single-post';
+export * from './user-profile';
--- libs/app/posts/ui/src/user-profile/user-profile.module.ts
+++ libs/app/posts/ui/src/user-profile/user-profile.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { UserProfileComponent } from './user-profile.component';
 
 
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    IonicModule,
+  ],
+  declarations: [UserProfileComponent],
+  exports: [UserProfileComponent]
 })
 export class UserProfileModule { }
